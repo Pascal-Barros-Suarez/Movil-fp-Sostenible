e8f1fb66b12e1447e9f71894fc795c5af9067e96
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-02-28 13:04:58 +0000

    organizacion de carpetas de react

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -6,14 +6,6 @@
 
 import './bootstrap';
 
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-//import './components/Example';
-
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 
