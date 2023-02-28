b75a485572826df28d9e807299171cbf7b640204
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-02-28 12:15:23 +0000

    inertia implementado perfectamente sin errores

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -12,7 +12,7 @@ import './bootstrap';
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-import './components/Example';
+//import './components/Example';
 
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
