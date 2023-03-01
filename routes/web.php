30f7552dac892f05b8a6e642ca026dc7a9bb27a8
AlexRod858 <oa.defrag@gmail.com>
2023-03-01 20:49:16 +0100

    wep php user comentado

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,9 @@ use App\Http\Controllers\UserController;
 }); */
 
 Route::get('/', function () {
-    $user = new User();
-    $user->name = "Mr. Bean";
-return Inertia::render('Home', ['user' => $user]);
+    // $user = new User();
+    // $user->name = "Mr. Bean";
+return Inertia::render('Home', /*['user' => $user]*/);
 });
 
 Auth::routes(['verify' => true]); //Activa la verificación en las rutas para laravel/ui
