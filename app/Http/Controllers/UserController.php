319ffaea8f2820c19a477569e640476fc3aa7b61
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-03-04 19:19:29 +0000

    edicion de usuarios terminada

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UserForm;
 
 class UserController extends Controller
 {
@@ -12,21 +14,31 @@ class UserController extends Controller
     {
         $this->middleware('auth');
     }
-    /*
-    public function edit()
-    {
-    $user = Auth::user();
-    return view('profile.edit', compact('user'));
-    }*/
 
     public function show()
     {
         return Inertia::render('UserProfile');
     }
 
-
-    public function logout()
+    public function editProfile(UserForm $request, User $user)
     {
-        //
+        // Valida los datos del formulario.
+        $validatedData = $request->validated();
+        $user = User::find(Auth::id());
+
+        // edicion de usuarios
+        $user->name = $validatedData['name'];
+        $user->age = $validatedData['age'];
+        $user->email = $validatedData['email'];
+        $user->smoker = $validatedData['smoker'];
+        // $user->password =  Hash::make($validatedData['password']);
+        $user->save();
+        $user->get();
+        // return Inertia::render('Profile/Index', [ 'users' => $user, 'success', 'Los datos se han actualizado correctamente']);
+        //devuelve el perfil del usuario 
+        //return Inertia::render('profile')->with('mensaje', 'Los datos se han actualizado correctamente');
+    }
+    function editPassword()
+    {
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -37,18 +37,18 @@ Route::get('/500', function () {
 });
 
 // USER //
-Route::post('/profile-edit', [UserController::class, 'editProfile'])->name('User.edit->Profile');
 Route::get('/profile', [UserController::class, 'show'])->name('User.show->Profile');
+Route::post('/profile-edit', [UserController::class, 'editProfile'])->name('User.edit->Profile');
 
 Auth::routes(['verify' => true]); //Activa la verificación en las rutas para laravel/ui
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
 
 
 //////////////////////////////
-Route::get('/travels', [TravelsController::class, 'index'])->name('travels.index');
+Route::get('/travels', [TravelsController::class, 'index'])->name('travels.index')->middleware(['auth', 'verified'])->name('travels');
 //F O R M //
-Route::get('/newride', [TravelsController::class, 'show'])->name('newride.form');
-Route::post('/newride', [TravelsController::class, 'create'])->name('travels.create');
+Route::get('/newride', [TravelsController::class, 'show'])->name('newride.form')->middleware(['auth', 'verified'])->name('newride.form');
+Route::post('/newride', [TravelsController::class, 'create'])->name('travels.create')->middleware(['auth', 'verified'])->name('newride');
 //////////////////////////////
 
 Auth::routes();
