d5635d17c78c8a736618032b066a4b62cb1b3e7e
AlexRod <you@example.com>
2023-03-05 07:26:57 +0000

    3 parte de 4 de mis viajes

--- resources/js/components/User-Profile/misviajes/Modals/ViewModal.jsx
+++ resources/js/components/User-Profile/misviajes/Modals/ViewModal.jsx
@@ -9,22 +9,22 @@ render(){
       // TypeError: Cannot read properties of undefined (reading 'classList'
       // id={'viewModal'+this.props.modalId}
       //THIS.PROPS.MODALID NO ME ESTÁ LINKEANDO CON TABLEACTIONBUTTONS
-      <div classname="modal fade" id={'viewModal'+this.props.modalId} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div classname="modal-dialog" role="document">
-        <div classname="modal-content">
-          <div classname="modal-header">
-            <h5 classname="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" classname="close" data-dismiss="modal" aria-label="Close">
+      <div class="modal fade" id={'viewModal'+this.props.modalId} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div classname="modal-body">
+          <div class="modal-body">
             Origen:  <strong>{this.props.viajeData.origin}</strong>
             ...
           </div>
-          <div classname="modal-footer">
-            <button type="button" classname="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" classname="btn btn-primary">Save changes</button>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
           </div>
         </div>
       </div>
