d3e1a01804d33d19065c3cc2596627648459f81f
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-03-08 03:11:26 +0000

    añadido de licencia MIT en footer y añadido de mensajes Flash

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,25 +1,18 @@
 import React from "react";
-import { usePage } from "@inertiajs/react";
-
 
 //componentes
-import Navigation from '../components/Navigation';
 // import Dashboard from '../components/Dashboard';
+import Navigation from '../components/Navigation';
 import MisViajes from '../components/misviajes/MisViajes';
 import Footer from '../components/Footer.jsx';
+import { dibujaFlash } from '../Helper';
 
 export default function Initiated() {
-    const { flash } = usePage().props;
 
-    function dibujaflash() {
-        let message = flash.success ? <h3 className="h-100 m-0 bg-success fa fa-flash">{flash.success}</h3> : '';
-        flash.success = null;
-        return message;
-    }
     return (
         <>
             <Navigation />
-            {dibujaflash()}
+            {dibujaFlash()}
             <MisViajes />
             <Footer />
         </>
